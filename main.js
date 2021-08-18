26254a95e80f97f164aafcc19cd79b57536879ef
kauanSampaio <kauansampaio53@gmail.com>
2021-08-18 19:16:18 -0300

    Main.js Update

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ const schema = {
 
 const store = new Store({ schema });
 
+//Quando o aplicativo estiver pronto, ele executará essa função que colocará o ícone no tray
 app.on('ready', () => {
+  //Criando um novo ícone no tray e colocando a imagem 'iconTemplate' como ícone
   const tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
   const projects = JSON.parse(store.get('projects'))
 
@@ -18,10 +20,11 @@ app.on('ready', () => {
   console.log(projects);
 
 
+  //Criando a opção 'Item1' no menu quando clicar no ícone no tray
   const contextMenu = Menu.buildFromTemplate([
     { label: 'Item1', type: 'radio', checked: true, click: () =>{
-      const [path] = dialog.showOpenDialog({ properties: ['openDirectory'] });
-
+      //Função realizada ao clicar 'Item1' no tray
+      const [path] = dialog.showOpenDialog({ properties: ['openDirectory'] }); //Mostra uma tela para o usuário abrir uma pasta e pega o caminho
       store.set('projects[]', JSON.stringify([ ... projects, path]));
     }}
   ]);
